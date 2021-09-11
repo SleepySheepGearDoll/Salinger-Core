1cbb007103d99b91266f5cc86212d7494f6b3da6
sleepy.sheep.gear.doll <sleepy.sheep.gear.doll@gmail.com>
2021-09-12 00:12:17 +0900

    Salinger-Core開発開始

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Salinger_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
